3fe7d6314f19fe616b7eb0df9174feeac4c94be9
yusnof <yusnofal@gmail.com>
2025-05-19 21:16:10 +0200

    started on myPlot to later plot the graphs with matlab

--- plot.py
+++ plot.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
 
-plt.plot(ypoints, marker='o')
-plt.show()
+class myPloty:
+ def __init__(self,xpos = [], ypos = []):
+  xpoints = np.array(xpos)
+  ypoints = np.array(ypos)
+  self.plot(xpoints,ypoints)
+
+ def plot(self,x,y):
+  plt.xlabel("File Size (kB)")
+  plt.ylabel("Time (S)") 
+  plt.grid()
+  plt.plot(x, y)
+  plt.show()
+
+py_list = [0.05, 0.09, 5.1, 5.3]
+g1 = myPloty(py_list,py_list)
